bf309911ab17e1580e72c1bb5ae3d46f8fd2eef1
Chang <tanteichang@outlook.com>
2017-04-27 23:21:08 +0800

    压缩image

--- part1/task7/gulpfile.js
+++ part1/task7/gulpfile.js
@@ -1,14 +1,20 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    imagemin = require('gulp-imagemin');
 
 gulp.task('sass', function () {
-  return gulp.src('*.scss')
+  return gulp.src('./src/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./dist/css'));
+});
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/*.scss', ['sass']);
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('*.scss', ['sass']);
+gulp.task('imagemin', function () {
+    gulp.src('src/images/*.{png,jpg,gif,ico}')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
 });
